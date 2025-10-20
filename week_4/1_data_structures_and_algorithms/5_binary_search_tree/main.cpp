8d0d28cb7fd42caa5f03212a7cf8ab36183b880e
Lafftale <lafftale1999@gmail.com>
2025-10-20 12:37:03 +0200

    update

--- week_4/1_data_structures_and_algorithms/5_binary_search_tree/main.cpp
+++ week_4/1_data_structures_and_algorithms/5_binary_search_tree/main.cpp
@@ -39,6 +39,30 @@ private:
         inorder(node->right);
     }
 
+    void preorder(const std::unique_ptr<Node>& node) const {
+        if(!node) return;
+        std::cout << node->data << " ";
+        preorder(node->left);
+        preorder(node->right);
+    }
+
+    void postorder(const std::unique_ptr<Node>& node) const {
+        if(!node) return;
+        postorder(node->left);
+        postorder(node->right);
+        std::cout << node->data << " ";
+    }
+
+    bool search(const std::unique_ptr<Node>& node, int value) const {
+        if (!node) return false;
+        if (node->data == value) return true;
+
+        if (value < node->data)
+            return search(node->left, value);
+        else
+            return search(node->right, value);
+    }
+
 public:
     BinarySearchTree() : root(nullptr), size(0) {}
     BinarySearchTree(std::initializer_list<int> elements)
@@ -56,8 +80,20 @@ public:
         inorder(root);
     }
 
+    void displayPreorder() const {
+        preorder(root);
+    }
+
+    void displayPostorder() const {
+        postorder(root);
+    }
+
     int getSize() const {
         return size;
+    }
+
+    bool find(int value) const {
+        return search(root, value);
     }
 };
 
@@ -65,6 +101,21 @@ int main(void) {
     BinarySearchTree bst = {3, 9, 23, 91, 2, 34, 8, 2, 5, 17, 24, 9, 1, 54, 76};
 
     bst.displayInorder();
+    bst.displayPreorder();
+    bst.displayPostorder();
+    
+    int numbers[] = {9, 21, 5, 76, 4};
+    for(auto& n : numbers) {
+        if (bst.find(n)) {
+            std::cout << n << " was found in the bst\n";
+        } else {
+            std:: cout << n << " was not found in the bst\n";
+        }
+    }
+
+    bst.insertElement(21);
+
+    if (bst.find(21)) std::cout << "21 was found in the bst!\n";
     std::cout << bst.getSize() << std::endl;
     return 0;
 }
