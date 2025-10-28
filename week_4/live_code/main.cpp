e855d6ebb3ea3e27a4c4fffc4f9f3dfd2ddac654
Lafftale <lafftale1999@gmail.com>
2025-10-28 11:10:42 +0100

    livecode sorted

--- week_4/live_code/main.cpp
+++ week_4/live_code/main.cpp
@@ -6,20 +6,37 @@
 #include "include/BinarySearchTree.hpp"
 #include "include/LinkedList.hpp" */
 
-/* int addNumber(int x, int y) {
+/*
+explanation why we use templates:
+instead of overloading the same function
+several times, we use templates - which under the hood
+will create the overloaded functions we use, but we
+don't need to write them!
+*/
+
+// This will only work properly for integers, there's no floating precision
+int addNumber(int x, int y) {
     return x + y;
 }
 
+// This will work with doubles aswell
 double addNumber(double x, double y) {
     return x + y;
-} */
+}
+
+// This will work for all datatypes that support addition
+template <typename T>
+T genericAdder(T x, T y) {
+    return x + y;
+}
+
 template <typename T, typename Y>
 Y divideTwoNumbers(T x, Y y) {
     return x / y;
 }
 
 template <typename T>
-T addTwoNumbers(std::vector<T> values) {
+T sumVector(std::vector<T> values) {
     T sum = 0;
     for(const auto& v : values) {
         sum += v;
@@ -33,6 +50,7 @@ double addTwoNumbers(double x, double y) {
     return x + y;
 }
 
+// ----------- Small showcasing of callback functions -----------
 void printNumber(int x) {
     std::cout << x << std::endl;
 }
@@ -44,8 +62,9 @@ void addition(int x, int y, void (*callback)(int)) {
 
 #include <algorithm>
 
-template <typename T>
-bool find_any_of(std::vector<T>& values, T target, bool(*compare)(T, T)) {
+// Templated search function
+template <typename T, typename Compare>
+bool find_any_of(std::vector<T>& values, T target, Compare compare) {
     for (const auto& v : values) {
         if(compare(v, target)) return true; 
     }
@@ -53,45 +72,29 @@ bool find_any_of(std::vector<T>& values, T target, bool(*compare)(T, T)) {
     return false;
 }
 
-class Person {
-public:
-    std::string name;
-};
-
-bool findPerson (Person& person) {
-    std::cout << person.name << std::endl;
-}
-
 int main(void) {
-    srand(NULL);
 
-/*     std::vector<int> values {3,9,21};
+    // Using templated functions to add numbers
+    std::vector<int> values {3,9,21};
+    std::cout << sumVector(values) << std::endl;
+    std::cout << addTwoNumbers(4.2, 9.3) << std::endl;
 
-    std::cout << addTwoNumbers(values) << std::endl;
-    std::cout << addTwoNumbers(4.2, 9.3) << std::endl; */
-
-    /* void (*callback)(int);
+    // Showcasing callbacks and function pointers
+    void (*callback)(int);
     callback = printNumber;
-    callback(53); */
+    callback(53);
 
+    // How we use STL algorithms and lambda functions to
+    // manipulate the containers
     std::vector<int> values = {3, 9, 82, 38, 49, 2, 7, 74};
-
-    std::sort(values.begin(), values.end(), [](const auto& a, const auto& b) {
+    std::sort(values.begin(), values.end(), [](const int& a, const int& b) {
         return a < b;
     });
 
     int target = 82;
 
-    if (find_any_of(values, 82, [](int a, int c) {
-        return a == c;
-    })) {
-        std::cout << "Target found\n";
-    } else {
-        std::cout << "Not found\n";
-    }
-
-    
-    /* if(std::any_of(values.begin(), values.end(), [&](const auto& a) {
+    // Using STL search algorithm and lambda to search in container
+    if(std::any_of(values.begin(), values.end(), [&](const auto& a) {
         return target == a;
     })) {
         std::cout << "Target found\n";
@@ -99,40 +102,22 @@ int main(void) {
         std::cout << "Not found\n";
     }
 
-    for (const auto& v : values) {
-        std::cout << v << ", ";
-    } */
+    // Using our own templated search function + lambda expression
+    if (find_any_of(values, 82, [](const int& a, const int& c) {
+        return a == c;
+    })) {
+        std::cout << "Target found\n";
+    } else {
+        std::cout << "Not found\n";
+    }
 
-    std::cout << std::endl;
-
+    // Passing lambda function as an argument to our function
     addition(24, 84, [](int n) {
         std::cout << "Lambda function callback: " << n << std::endl;
     });
 
-    
-
-    /* std::vector<std::string> first_names = {"Carl", "Hussein", "Ava", "Richard"};
-    std::vector<std::string> last_names = {"Sevor", "River", "Lovefruit", "Grip"};
-
-    std::vector<std::string> names;
-
-    int fn_size = first_names.size();
-    int ln_size = last_names.size();
-
-    for(int i = 0; i < 30; i++) {
-        int fn = rand() % fn_size;
-        int ln = rand() % ln_size;
-
-        std::string name = first_names.at(fn) + " " + last_names.at(ln);
-        names.push_back(name);
-    }
-
-    for (const auto& name : names) {
-        std::cout << name << std::endl;
-    } */
-
-    /* std::vector<int> values = {3,9,2,1};
-
+    // Showcasing how the vector behaves when adding and ereasing elements
+    std::vector<int> values = {3,9,2,1};
     std::cout << "Before adding\n";
     for(auto v : values) {
         std::cout << v << std::endl;
@@ -150,7 +135,7 @@ int main(void) {
     std::cout << "After deleting\n";
     for(auto v : values) {
         std::cout << v << std::endl;
-    } */
+    }
 
     return 0;
 }
